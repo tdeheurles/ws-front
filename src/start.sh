80d71d24aa8949fd28bcc63af7439a13f8df3afb
tdeheurles <tdeheurles@gmail.com>
2015-06-29 17:27:57 +0200

    Removed TARGET_SERVICE from site_ssl.conf

--- src/start.sh
+++ src/start.sh
@@ -9,24 +9,24 @@ else
   cp /usr/src/site_nossl.conf /etc/nginx/conf.d/site.conf
 fi
 
-# If an htpasswd file is provided, download and configure nginx
-if [ -n "${ENABLE_BASIC_AUTH+1}" ] && [ "${ENABLE_BASIC_AUTH,,}" = "true" ]; then
-  echo "Enabling basic auth..."
-   sed -i "s/#auth_basic/auth_basic/g;" /etc/nginx/conf.d/proxy.conf
-fi
-
-# If the SERVICE_HOST_ENV_NAME and SERVICE_PORT_ENV_NAME vars are provided,
-# they point to the env vars set by Kubernetes that contain the actual
-# target address and port. Override the default with them.
-if [ -n "${SERVICE_HOST_ENV_NAME+1}" ]; then
-  TARGET_SERVICE=${!SERVICE_HOST_ENV_NAME}
-fi
-if [ -n "${SERVICE_PORT_ENV_NAME+1}" ]; then
-  TARGET_SERVICE="$TARGET_SERVICE:${!SERVICE_PORT_ENV_NAME}"
-fi
-
-# Tell nginx the address and port of the service to proxy to
-sed -i "s/{{TARGET_SERVICE}}/${TARGET_SERVICE}/g;" /etc/nginx/conf.d/proxy.conf
+# # If an htpasswd file is provided, download and configure nginx
+# if [ -n "${ENABLE_BASIC_AUTH+1}" ] && [ "${ENABLE_BASIC_AUTH,,}" = "true" ]; then
+#   echo "Enabling basic auth..."
+#    sed -i "s/#auth_basic/auth_basic/g;" /etc/nginx/conf.d/site.conf
+# fi
+#
+# # If the SERVICE_HOST_ENV_NAME and SERVICE_PORT_ENV_NAME vars are provided,
+# # they point to the env vars set by Kubernetes that contain the actual
+# # target address and port. Override the default with them.
+# if [ -n "${SERVICE_HOST_ENV_NAME+1}" ]; then
+#   TARGET_SERVICE=${!SERVICE_HOST_ENV_NAME}
+# fi
+# if [ -n "${SERVICE_PORT_ENV_NAME+1}" ]; then
+#   TARGET_SERVICE="$TARGET_SERVICE:${!SERVICE_PORT_ENV_NAME}"
+# fi
+#
+# # Tell nginx the address and port of the service to proxy to
+# sed -i "s/{{TARGET_SERVICE}}/${TARGET_SERVICE}/g;" /etc/nginx/conf.d/site.conf
 
 echo "Starting nginx..."
 nginx -g 'daemon off;'
